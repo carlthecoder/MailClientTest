84ac31cd0e566885c17e86cea9ec6bd4382cae57
carldev47 <carldev47@outlook.com>
2020-12-23 15:07:14 +0100

    tmp save mailclient

---
+++ CSharpDevTest/DeveloperTest/Model/MailService.cs
@@ -0,0 +1,38 @@
+﻿using Limilabs.Client.IMAP;
+using Limilabs.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeveloperTest.Model
+{
+    public sealed class MailService : IMailService
+    {
+        // Todo: input correct algorithm (maybe a strategy pattern here..)
+        public async Task ConnectAsync(string servername, int port, string username, string password)
+        {
+            await Task.Run(() =>
+            {
+                using (var imap = new Imap())
+                {
+                    imap.SSLConfiguration.EnabledSslProtocols = SslProtocols.Tls12;
+                    imap.ConnectSSL(servername, port);  // or ConnectSSL for SSL
+                    imap.UseBestLogin("carl.claessens@gmail.com", "Gm@ilpas13");
+                    imap.SelectInbox();
+                    var folders = imap.GetFolders();
+                    List<long> uids = imap.Search(Flag.All);
+                    foreach (long uid in uids)
+                    {
+                        IMail email = new MailBuilder()
+                            .CreateFromEml(imap.GetMessageByUID(uid));
+                        Console.WriteLine(email.Subject);
+                    }
+                    imap.Close();
+                }
+            }).ConfigureAwait(false);
+        }
+    }
+}
---
+++ CSharpDevTest/DeveloperTest/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,137 @@
+﻿using DeveloperTest.Model;
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+
+namespace DeveloperTest.ViewModel
+{
+    public class MainWindowViewModel : IMainWindowViewModel
+    {
+        private readonly IMailService mailService;
+
+        private string servername;
+        private string port;
+        private string username;
+        private string password;
+        private ConnectionType selectedConnection = ConnectionType.IMAP;
+        private EncryptionType selectedEncryption = EncryptionType.SSL_TLS;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public IEnumerable ConnectionTypes => Enum.GetValues(typeof(ConnectionType));
+        public IEnumerable EncryptionTypes => Enum.GetValues(typeof(EncryptionType));
+
+        public ConnectionType SelectedConnection
+        {
+            get { return selectedConnection; }
+            set
+            {
+                if (value == selectedConnection)
+                    return;
+
+                selectedConnection = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public EncryptionType SelectedEncryption
+        {
+            get { return selectedEncryption; }
+            set
+            {
+                if (value == selectedEncryption)
+                    return;
+
+                selectedEncryption = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Servername
+        {
+            get { return servername; }
+            set
+            {
+                if (value == servername)
+                    return;
+
+                servername = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(StartCommand));
+            }
+        }
+
+        public string Port
+        {
+            get { return port; }
+            set
+            {
+                if (value == port)
+                    return;
+                if (!Regex.IsMatch(value, "^\\d+$"))
+                    return;
+
+                port = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(StartCommand));
+            }
+        }
+
+        public string Username
+        {
+            get { return username; }
+            set
+            {
+                if (value == username)
+                    return;
+
+                username = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(StartCommand));
+            }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                if (value == password)
+                    return;
+
+                password = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(StartCommand));
+            }
+        }
+
+        public ICommand StartCommand => new RelayCommand(Start, CanStart);
+
+        public MainWindowViewModel()
+        {
+            mailService = new MailService();
+        }
+
+        private bool CanStart(object obj)
+        {
+            return !string.IsNullOrEmpty(Password) &&
+                    !string.IsNullOrEmpty(Username) &&
+                    !string.IsNullOrEmpty(Servername) &&
+                    !string.IsNullOrEmpty(Port);
+        }
+
+        private void Start(object obj)
+        {
+            int port = int.Parse(Port);
+            mailService.ConnectAsync(Servername, port, Username, Password);
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
