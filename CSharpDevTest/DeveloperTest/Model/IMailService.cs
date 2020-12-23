e60b7abbc8916cb736de5546e1fb6e99832abd5f
carldev47 <carldev47@outlook.com>
2020-12-23 15:14:24 +0100

    fds

---
+++ CSharpDevTest/DeveloperTest/Model/IMailService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace DeveloperTest.Model
+{
+    public interface IMailService
+    {
+        Task ConnectAsync(string servername, int port, string username, string password);
+
+    }
+}
